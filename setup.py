3bf011aba38025b9c858437cfc2e8400da27cec3
Christa <christabelle@student.ie.edu>
2016-06-19 20:13:49 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fuzzydata',
+    version='0.1',
+    description='Final Financial Analytics Project Group H',
+    url='https://github.com/christabelle/fuzzydata_GroupH',
+    download_url='',
+    author='Group-H',
+    author_email='christabelle@student.ie.edu',
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords=['final', 'financial', 'analytics', 'project'],
+    packages=['fuzzydata']
+)
